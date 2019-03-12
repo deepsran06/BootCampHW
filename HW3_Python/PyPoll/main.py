b9b2549e5ad5092e1cbb07ac27cf870f16413055
Deep Sran <deepsran06@gmail.com>
2019-03-12 11:16:05 -0700

    for statement for votes

--- HW3_Python/PyPoll/main.py
+++ HW3_Python/PyPoll/main.py
@@ -1,15 +1,28 @@
-# PyPoll Solutions using dictionaries, lists, and lambda function
+# PyPoll Solution using dictionaries, lists, and lambda function
 
 # Import Dependencies
 
 import csv
 import os
 
-# Set and Read file path
+# Define varibles using dictionaries and lists
 
+candidates = []
+vote_count = {}
+vote_percent = {}
+votes = -1
+
+# Set and Read file path
 file = os.path.join("Resources/election_data.csv")
 with open(file, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
-
+# Use for and if to count the number of votes using append
+    for row in csvreader:
+        votes = votes + 1
+            if row[2] not in candidates and row[2] not in "Candidate":
+                candidates.append(row[2])
+                vote_count[row[2]] = 1
+            elif row[2] in candidates and row[2] not in "Candidate":
+            vote_count[row[2]] = vote_count[row[2]] + 1
 
