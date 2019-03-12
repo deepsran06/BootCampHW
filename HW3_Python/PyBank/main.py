1079387bb03b25b1039403dfce5670a46622abcf
Deep Sran <deepsran06@gmail.com>
2019-03-12 11:50:01 -0700

    read csv file

--- HW3_Python/PyBank/main.py
+++ HW3_Python/PyBank/main.py
@@ -5,3 +5,7 @@
 import csv
 import os
 
+# Set and Read file path
+file = os.path.join("Resources/budget_data.csv")
+with open(file, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
