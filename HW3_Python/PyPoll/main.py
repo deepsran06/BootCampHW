0f9d80da0c26ca2eb0f13340b7bd456444484523
Deep Sran <deepsran06@gmail.com>
2019-03-12 11:20:02 -0700

    fix indentation, add logic for vote percentage

--- HW3_Python/PyPoll/main.py
+++ HW3_Python/PyPoll/main.py
@@ -20,9 +20,17 @@ with open(file, newline="") as csvfile:
 # Use for and if to count the number of votes using append
     for row in csvreader:
         votes = votes + 1
-            if row[2] not in candidates and row[2] not in "Candidate":
-                candidates.append(row[2])
-                vote_count[row[2]] = 1
-            elif row[2] in candidates and row[2] not in "Candidate":
+        if row[2] not in candidates and row[2] not in "Candidate":
+            candidates.append(row[2])
+            vote_count[row[2]] = 1
+        elif row[2] in candidates and row[2] not in "Candidate":
             vote_count[row[2]] = vote_count[row[2]] + 1
+
+# Calculations for Percentage of Vote
+    for name, num in vote_count.items():
+        vote_percent[name] = str(round(((num/votes)*100),3)) + "% ("+str(num) + ")"
+
+
+
+
 
