e565221fb231b30c07d4453e5dcfa3183b8367e1
Deep Sran <deepsran06@gmail.com>
2019-04-23 17:41:51 -0700

    solutions for 3

--- HW7_SQL/Solved.sql
+++ HW7_SQL/Solved.sql
@@ -38,4 +38,17 @@ SELECT country_id 'Country ID', country 'Country'
 FROM country
 WHERE country IN ('Afghanistan', 'Bangladesh', 'China');
 
-## 
+## 3a. You want to keep a description of each actor. 
+## You don't think you will be performing queries on a description, 
+## so create a column in the table `actor` named `description` 
+## and use the data type `BLOB` (Make sure to research the type `BLOB`, 
+## as the difference between it and `VARCHAR` are significant)
+ALTER TABLE actor
+ADD COLUMN description BLOB AFTER last_name;
+
+## 3b. Very quickly you realize that entering descriptions for each actor is too much effort. 
+## Delete the `description` column
+ALTER TABLE actor
+DROP COLUMN description;
+
+## 4a. List the last names of actors, as well as how many actors have that last name.
