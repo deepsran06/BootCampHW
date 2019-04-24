b7ca377d6905dfb3eaa274fe004b5d13b705674a
Deep Sran <deepsran06@gmail.com>
2019-04-23 17:25:19 -0700

    first commit

---
+++ HW7_SQL/Solved.sql
@@ -0,0 +1,8 @@
+## SQL Homework
+
+## Use create database
+USE sakila;
+
+## 1a. Display the first and last names of all actors from the table 'actor'
+SELECT first_name 'First Name', last_name 'Last Name'
+FROM actor;
